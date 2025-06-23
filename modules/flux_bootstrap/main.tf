3260a9a023c1254f6758fa0cb05ae2649063f302
tabulat <izotov.andriy@gmail.com>
2025-06-23 13:48:18 +0000

    add files flux_bootstrap

--- modules/flux_bootstrap/main.tf
+++ modules/flux_bootstrap/main.tf
@@ -1,16 +1,7 @@
 provider "flux" {
   kubernetes = {
-    config_path = var.config_path
+    host                   = var.config_host
+    token                  = var.config_token
+    cluster_ca_certificate = var.config_ca
   }
-  git = {
-    url = "https://github.com/${var.github_repository}.git"
-    http = {
-      username = "git"
-      password = var.github_token
-    }
-  }
-}
-
-resource "flux_bootstrap_git" "this" {
-  path = var.target_path
 }
