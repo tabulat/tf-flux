e5f431ca72a1a111a1cb26b397d8245c817eb82b
tabulat <izotov.andriy@gmail.com>
2025-06-23 11:55:56 +0000

    add files

--- modules/flux_bootstrap/variables.tf
+++ modules/flux_bootstrap/variables.tf
@@ -3,12 +3,6 @@ variable "github_repository" {
   description = "GitHub repository to store Flux manifests"
 }
 
-variable "flux_namespace" {
-  type        = string
-  default     = "flux-system"
-  description = "GitHub repository to store Flux manifests"
-}
-
 variable "target_path" {
   type        = string
   default     = "clusters"
@@ -26,20 +20,8 @@ variable "private_key" {
   description = "The private key used to authenticate with the Git repository"
 }
 
-variable "config_host" {
+variable "config_path" {
   type        = string
-  default     = "gke"
-  description = "The url for gke"
-}
-
-variable "config_token" {
-  type        = string
-  default     = "token"
-  description = "The token for gke"
-}
-
-variable "config_ca" {
-  type        = string
-  default     = "ca"
-  description = "The ca for gke"
+  default     = "~/.kube/config"
+  description = "The path to the kubeconfig file"
 }
